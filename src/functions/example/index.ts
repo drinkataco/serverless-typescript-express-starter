eeeb1e182afad13e61e62ab469ca3fe33d605da9
J J Walwyn <joshwalwyn@economist.com>
2021-08-13 23:24:49 +0100

    Initial Severless Typescript Boilerplate

---
+++ src/functions/example/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import serverless from 'serverless-http';
+
+const app = express();
+
+app.get('/example', (_req, res) =>
+  res.status(200).json({
+    message: 'This is an example response',
+  })
+);
+
+export default serverless(app);
+
