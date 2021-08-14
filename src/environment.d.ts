62700fb738560c0065a4b0ad29d8a742bc90a0fb
J J Walwyn <joshwalwyn@economist.com>
2021-08-14 14:43:26 +0100

    Introduce linting and prettier

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
@@ -8,4 +9,4 @@ declare global {
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export {}
+export {};
--- src/functions/example/index.ts
+++ src/functions/example/index.ts
@@ -3,11 +3,8 @@ import serverless from 'serverless-http';
 
 const app = express();
 
-app.get('/example', (_req, res) =>
-  res.status(200).json({
-    message: 'This is an example response',
-  })
-);
+app.get('/example', (_req, res) => res.status(200).json({
+  message: 'This is an example response',
+}));
 
 export default serverless(app);
-
