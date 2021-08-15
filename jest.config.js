6d24d5ce8b1e482def49130482a89ae1923ac0b8
J J Walwyn <joshwalwyn@economist.com>
2021-08-15 16:23:07 +0100

    Added default coverage rules to 100%

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,17 @@ module.exports = {
     '**/?(*.)test.ts',
   ],
   testPathIgnorePatterns: ['node_modules', '.cache', 'public'],
-}
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
