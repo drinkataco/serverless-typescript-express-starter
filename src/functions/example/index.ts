121e7be66b5deef76d145646bf0335fb38c741f1
J J Walwyn <me@joshwalwyn.com>
2021-08-16 20:22:49 +0100

    Adding types

--- src/functions/example/index.ts
+++ src/functions/example/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import serverless from 'serverless-http';
 
-const app = express();
+const app: express.Application = express();
 
-app.get('/example', (_req, res) => res.status(200).json({
-  message: 'This is an example response',
-}));
+app.get('/example', (_req: express.Request, res: express.Response) =>
+  res.status(200).json({
+    message: 'This is an example response',
+  }),
+);
 
 export default serverless(app);
